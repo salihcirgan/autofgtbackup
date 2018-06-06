f4d8a6fa46c11ba03b95080bb6823b8aed6f4895
Salih CIRGAN <salih@cirgan.net>
2018-06-06 14:31:35 +0300

    replace quotation marks

--- backup.ex
+++ backup.ex
@@ -6,11 +6,11 @@ set FGTNAME [lindex $argv 0]
 set FGTIP [lindex $argv 1]
 set timeout 8000
 #Enter your FTP Server IP Address without quotes
-set FTP_SERVER_IP “IP Address”
+set FTP_SERVER_IP "IP Address"
 #Enter your FTP Server Username without quotes
-set FTP_USER_NAME “ftpusername”
+set FTP_USER_NAME "ftpusername"
 #Enter your FTP Server Password without quotes
-set FTP_PASSWORD “ftppassword”
+set FTP_PASSWORD "ftppassword"
 #This will be our date format while writing backup file
 set date [clock format [clock seconds] -format {%d-%m-%Y}]
 
@@ -19,17 +19,17 @@ spawn ssh admin@$FGTIP
 
 expect {
 
-“password:” {
-send “yourFGTpassword\r”}
-“(yes/no)? ” {
-send “yes\r”
-expect “password:” {
-send “youFGTpassword\r”}}
+"password:" {
+send "yourFGTpassword\r"}
+"(yes/no)? " {
+send "yes\r"
+expect "password:" {
+send "youFGTpassword\r"}}
 }
 
-expect “# ”
-send “config global\r”
-expect “# ”
-send “execute backup full-config ftp /ftppath/$FGTNAME-$date.conf $FTP_SERVER_IP $FTP_USER_NAME $FTP_PASSWORD\r”
-expect “# ”
-send “exit\r”
+expect "# "
+send "config global\r"
+expect "# "
+send "execute backup full-config ftp /ftppath/$FGTNAME-$date.conf $FTP_SERVER_IP $FTP_USER_NAME $FTP_PASSWORD\r"
+expect "# "
+send "exit\r"
